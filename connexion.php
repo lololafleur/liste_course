f2d8e47503b90f9e2dca93f20adc4e0f341d5d97
lololafleur <sguitton2@gmail.com>
2017-11-24 06:13:44 +0100

    requete ajout

--- connexion.php
+++
@@ -1,3 +0,0 @@
-<?php
-$bdd = new PDO('mysql:host=localhost;dbname=liste_course;charset=utf8', 'root', '');
-
--- pdo/connexion.php
+++
@@ -1,3 +0,0 @@
-<?php
-$bdd = new PDO('mysql:host=localhost;dbname=formation;charset=utf8', 'root', '');
-
